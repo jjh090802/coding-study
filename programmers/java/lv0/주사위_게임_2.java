4ab7e2f9933fd62a50f65f3b017880d3f568af1c
jjh090802 <j01092014477@gmail.com>
2026-09-15 15:33:23 +0900

    fix: 주사위 게임 2 클래스 래핑 및 java sourcePaths 설정 수정

--- programmers/java/lv0/주사위_게임_2.java
+++ programmers/java/lv0/주사위_게임_2.java
@@ -2,13 +2,15 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/181930
 // 요약: a,b,c의 값들을 비교하여 그에 따라 출력값을 변환
 
-int solution(int a, int b, int c) {
-    int answer = 0;
-        
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+
         if (a == b && b == c) {answer = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);}
 
         else if (a != b && b != c && a != c) {answer = a + b + c;}
         else {answer = (a + b + c) * (a * a + b * b + c * c);}
-        
+
         return answer;
+    }
 }
